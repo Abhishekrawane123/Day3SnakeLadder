835cff8c358dc833eaeb7aaa6526750b71453af1
pknowledge <abhishekrawane1195@gmail.com>
2021-11-01 11:31:11 +0530

    second commit

--- src/com/bridgelabz/Snake_Ladder/Snake_Ladders.java
+++ src/com/bridgelabz/Snake_Ladder/Snake_Ladders.java
@@ -4,9 +4,9 @@ public class Snake_Ladders {
 
 	public static void main(String[] args) {
 
-		int player = 0;
-		System.out.print("Player intitial position: " + player);
 
+		int dice = (int) ((Math.random() * 10) % 6) + 1;
+		System.out.println("Dice is rolled " + dice);
 	}
 
 }
