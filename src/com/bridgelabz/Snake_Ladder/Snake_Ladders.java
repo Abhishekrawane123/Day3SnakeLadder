d4f4577b0584ee938161e5f54b77ae1cf390d27d
pknowledge <abhishekrawane1195@gmail.com>
2021-11-01 11:11:53 +0530

    first commit

---
+++ src/com/bridgelabz/Snake_Ladder/Snake_Ladders.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.Snake_Ladder;
+
+public class Snake_Ladders {
+
+	public static void main(String[] args) {
+
+		int player = 0;
+		System.out.print("Player intitial position: " + player);
+
+	}
+
+}
