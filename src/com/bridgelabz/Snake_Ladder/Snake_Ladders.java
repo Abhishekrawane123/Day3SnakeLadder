d9a1a1922481994955051077f2368dd43a9a6761
pknowledge <abhishekrawane1195@gmail.com>
2021-11-01 11:46:12 +0530

    fourth commit

--- src/com/bridgelabz/Snake_Ladder/Snake_Ladders.java
+++ src/com/bridgelabz/Snake_Ladder/Snake_Ladders.java
@@ -9,6 +9,9 @@ public class Snake_Ladders {
 		final int snake = 1;
 		final int ladder = 2;
 		int player = 0;
+		
+		while(player<100) {
+		
 		int dice = (int) ((Math.random() * 10) % 6) + 1;
 
 		int act = (int) ((Math.random() * 10) % 3);
@@ -18,16 +21,22 @@ public class Snake_Ladders {
 		case NO_PLAY:
 			break;
 		case snake:
-			player = player - dice;
+			int temp = player - dice;
+			if(temp<0) {
+				player=0;
+			}
+			else 
+				player=temp;
 			break;
 		case ladder:
 			player = player + dice;
 			break;
 		
 		}
-
+		}
 		System.out.println("player position " + player);
-
+	
 	}
-
 }
+
+
